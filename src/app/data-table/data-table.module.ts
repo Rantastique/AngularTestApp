40eb880a70ff328970573e0dd2c45ba7c96dbff3
Sabrina <reichert@zwo-null.de>
2020-12-14 20:36:52 +0100

    added dashboard and data-table modules

---
+++ src/app/data-table/data-table.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DataTableRoutingModule } from './data-table-routing.module';
+import { DataTableComponent } from './data-table.component';
+
+
+@NgModule({
+  declarations: [DataTableComponent],
+  imports: [
+    CommonModule,
+    DataTableRoutingModule
+  ]
+})
+export class DataTableModule { }
