05add45165fbad807459c31894b43ec20d71669a
Sabrina <reichert@zwo-null.de>
2020-12-15 15:20:12 +0100

    moved dummy data from component to new service

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -2,80 +2,7 @@ import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatSort} from '@angular/material/sort';
 import {SelectionModel} from '@angular/cdk/collections';
-/*
-just get some dummy data for now
- */
-export interface Person {
-  id: number;
-  first_name: string;
-  last_name: string;
-  email: string;
-  catchphrase: string;
-}
-
-const PERSON_DATA: Person[] = [
-  {
-    id: 1,
-    first_name: 'Matti',
-    last_name: 'Corrington',
-    email: 'mcorrington0@symantec.com',
-    catchphrase: 'Synergistic analyzing benchmark'
-  }, {
-    id: 2,
-    first_name: 'Agneta',
-    last_name: 'Feeney',
-    email: 'afeeney1@gov.uk',
-    catchphrase: 'Multi-layered maximized alliance'
-  }, {
-    id: 3,
-    first_name: 'Dalila',
-    last_name: 'Regina',
-    email: 'dregina2@answers.com',
-    catchphrase: 'Enhanced executive algorithm'
-  }, {
-    id: 4,
-    first_name: 'Hodge',
-    last_name: 'Giacomuzzo',
-    email: 'hgiacomuzzo3@lycos.com',
-    catchphrase: 'Synergized asymmetric model'
-  }, {
-    id: 5,
-    first_name: 'Zacharias',
-    last_name: 'Le Grice',
-    email: 'zlegrice4@sphinn.com',
-    catchphrase: 'Future-proofed web-enabled protocol'
-  }, {
-    id: 6,
-    first_name: 'Quinn',
-    last_name: 'Heibel',
-    email: 'qheibel5@skyrock.com',
-    catchphrase: 'Realigned fault-tolerant capability'
-  }, {
-    id: 7,
-    first_name: 'Corey',
-    last_name: 'Robinett',
-    email: 'crobinett6@de.vu',
-    catchphrase: 'Organic multi-tasking matrix'
-  }, {
-    id: 8,
-    first_name: 'Ameline',
-    last_name: 'Scolts',
-    email: 'ascolts7@china.com.cn',
-    catchphrase: 'Automated modular collaboration'
-  }, {
-    id: 9,
-    first_name: 'Jedediah',
-    last_name: 'Klein',
-    email: 'jklein8@odnoklassniki.ru',
-    catchphrase: 'Face to face coherent framework'
-  }, {
-    id: 10,
-    first_name: 'Karina',
-    last_name: 'Dencs',
-    email: 'kdencs9@narod.ru',
-    catchphrase: 'Total bandwidth-monitored strategy'
-  }
-];
+import {Person, PersonDataService} from '../person-data.service';
 
 @Component({
   selector: 'app-data-table',
@@ -83,19 +10,28 @@ const PERSON_DATA: Person[] = [
   styleUrls: ['./data-table.component.scss']
 })
 export class DataTableComponent implements OnInit, AfterViewInit {
-  personDataSource = new MatTableDataSource(PERSON_DATA);
+  personData: Array<Person> = [];
+  personDataSource: MatTableDataSource<Person>;
   columnsToDisplay: string[] = ['select', 'id', 'first_name', 'last_name', 'email', 'catchphrase'];
   selection = new SelectionModel<Person>(true, []);
 
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor() { }
+  constructor(private personDataService: PersonDataService) {
+    this.personData = this.getPersonData();
+    this.personDataSource = new MatTableDataSource(this.personData);
+  }
 
   ngOnInit(): void {
+
   }
 
   ngAfterViewInit(): void {
     this.personDataSource.sort = this.sort;
+  }
+
+  getPersonData(): Array<Person> {
+    return this.personDataService.getPersonData();
   }
 
   // Directly taken from https://material.angular.io/components/table/overview#selection
