2fa357d3a327913381b8bddd730cc51726f0e690
Sabrina <reichert@zwo-null.de>
2020-12-15 13:20:27 +0100

    basic table design and dummy data

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -1,4 +1,78 @@
 import { Component, OnInit } from '@angular/core';
+/*
+just get some dummy data for now
+ */
+export interface Person {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  catchphrase: string;
+}
+
+const PERSON_DATA: Person[] = [
+  {
+    id: 1,
+    first_name: 'Matti',
+    last_name: 'Corrington',
+    email: 'mcorrington0@symantec.com',
+    catchphrase: 'Synergistic analyzing benchmark'
+  }, {
+    id: 2,
+    first_name: 'Agneta',
+    last_name: 'Feeney',
+    email: 'afeeney1@gov.uk',
+    catchphrase: 'Multi-layered maximized alliance'
+  }, {
+    id: 3,
+    first_name: 'Dalila',
+    last_name: 'Regina',
+    email: 'dregina2@answers.com',
+    catchphrase: 'Enhanced executive algorithm'
+  }, {
+    id: 4,
+    first_name: 'Hodge',
+    last_name: 'Giacomuzzo',
+    email: 'hgiacomuzzo3@lycos.com',
+    catchphrase: 'Synergized asymmetric model'
+  }, {
+    id: 5,
+    first_name: 'Zacharias',
+    last_name: 'Le Grice',
+    email: 'zlegrice4@sphinn.com',
+    catchphrase: 'Future-proofed web-enabled protocol'
+  }, {
+    id: 6,
+    first_name: 'Quinn',
+    last_name: 'Heibel',
+    email: 'qheibel5@skyrock.com',
+    catchphrase: 'Realigned fault-tolerant capability'
+  }, {
+    id: 7,
+    first_name: 'Corey',
+    last_name: 'Robinett',
+    email: 'crobinett6@de.vu',
+    catchphrase: 'Organic multi-tasking matrix'
+  }, {
+    id: 8,
+    first_name: 'Ameline',
+    last_name: 'Scolts',
+    email: 'ascolts7@china.com.cn',
+    catchphrase: 'Automated modular collaboration'
+  }, {
+    id: 9,
+    first_name: 'Jedediah',
+    last_name: 'Klein',
+    email: 'jklein8@odnoklassniki.ru',
+    catchphrase: 'Face to face coherent framework'
+  }, {
+    id: 10,
+    first_name: 'Karina',
+    last_name: 'Dencs',
+    email: 'kdencs9@narod.ru',
+    catchphrase: 'Total bandwidth-monitored strategy'
+  }
+];
 
 @Component({
   selector: 'app-data-table',
@@ -6,6 +80,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-table.component.scss']
 })
 export class DataTableComponent implements OnInit {
+  personDataSource = PERSON_DATA;
+  columnsToDisplay: string[] = ['id', 'first_name', 'last_name', 'email', 'catchphrase'];
 
   constructor() { }
 
--- src/app/data-table/data-table.module.ts
+++ src/app/data-table/data-table.module.ts
@@ -4,14 +4,16 @@ import { CommonModule } from '@angular/common';
 import { DataTableRoutingModule } from './data-table-routing.module';
 import { DataTableComponent } from './data-table.component';
 import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
 
 
 @NgModule({
   declarations: [DataTableComponent],
-    imports: [
-        CommonModule,
-        DataTableRoutingModule,
-        MatButtonModule
-    ]
+  imports: [
+    CommonModule,
+    DataTableRoutingModule,
+    MatButtonModule,
+    MatTableModule
+  ]
 })
 export class DataTableModule { }
