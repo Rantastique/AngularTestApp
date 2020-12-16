1b5d2985bcb83a14f84da66c1d4e84b6df81be80
Sabrina <reichert@zwo-null.de>
2020-12-16 16:26:22 +0100

    fetch data when app starts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PersonDataService} from './person-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'testaufgabe';
+export class AppComponent implements OnInit{
+  constructor(private personDataService: PersonDataService) {
+  }
+
+  ngOnInit(): void {
+    this.personDataService.fetchPersonData();
+  }
 }
